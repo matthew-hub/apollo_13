c0fb91cc596b7c20db2f013e4dc6d52216db5124
matthew-hub <mateuszr430@gmail.com>
2020-04-14 19:06:52 +0200

    create Phase component

---
+++ src/components/Phase.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="phase-box">
+    <div class="phase-number">Phase {{ phase.phase }}:</div>
+    <div class="phase-additional" v-if="phase.additional">{{ phase.additional }}</div>
+    <div class="phase-description">{{ phase.description }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Phase",
+  props: {
+    phase: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.phase-box {
+  color: #202020;
+  .phase-number {
+    font-size: 2rem;
+    font-family: BlackHanSans;
+  }
+  .phase-additional {
+    font-family: Anton;
+    font-size: 1.8rem;
+  }
+  .phase-description {
+    color: #181818;
+    font-family: OpenSans;
+    font-size: 1.8rem;
+  }
+}
+</style>
