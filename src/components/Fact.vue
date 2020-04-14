b227fdd388b740336186eb034dac0b2e408d4b1b
matthew-hub <mateuszr430@gmail.com>
2020-04-14 19:06:42 +0200

    create Fact component

---
+++ src/components/Fact.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="fact-box">
+    <div class="fact-text">{{ fact.fact }}</div>
+    <div class="fact-description">{{ fact.description }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Fact",
+  props: {
+    fact: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.fact-box {
+  color: #202020;
+  .fact-text {
+    font-family: Anton;
+    font-size: 1.8rem;
+  }
+  .fact-description {
+    // color: #181818;
+    font-family: BlackHanSans;
+    font-size: 1.8rem;
+  }
+}
+</style>
